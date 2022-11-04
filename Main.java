bb98ed89f0e00ea12af0f9b27216d69fdd66fa05
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-04 11:17:20 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,54 +3,24 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 	
-		int year,chinaZodiac;
-		String zodiac="";
+		int year,divideYear;
 		Scanner input=new Scanner(System.in);
-		System.out.print("Doğum yılınızı giriniz:");
+		System.out.print("Yıl giriniz:");
 		year=input.nextInt();
-		
-		chinaZodiac = year % 12;
-		if(chinaZodiac==0)  {
-			zodiac="Maymun";
+		divideYear=year/100;
+		if (year%100==0) {
+			 if (divideYear % 4==0 )  {
+				System.out.println(  year + " yılı bir artık yıldır!");
 		}
-		
-		else if(chinaZodiac==1){
-			zodiac="Horoz" ;
+			 else {
+				 System.out.println(  year + " yılı bir artık yıl değildir!");
+			 }
+			}
+		else  { 
+			if (year % 4 == 0) {
+			System.out.println(  year + " yılı bir artık yıldır!");}
+			else {
+			System.out.println(  year + " yılı bir artık yıl değildir!");
 		}
-		else if(chinaZodiac==2){
-			zodiac="Köpek";
 		}
-		else if(chinaZodiac==3){
-			zodiac="Domuz";
-		}
-		else if(chinaZodiac==4){
-			zodiac="Fare";
-		}
-		else if(chinaZodiac==5){
-			zodiac="Öküz";
-		}
-		else if(chinaZodiac==6){
-			zodiac="Kaplan";
-		}
-		else if(chinaZodiac==7){
-			zodiac="Tavşan";
-		}
-		else if(chinaZodiac==8){
-			zodiac="Ejderha";
-		}
-		else if(chinaZodiac==9){
-			zodiac="Yılan";
-		}
-		else if(chinaZodiac==10){
-			zodiac="At";
-		}
-		else if(chinaZodiac==11){
-			zodiac="Koyun";
-		}
-		
-		System.out.println("Çin zodyağı burcunuz:" + zodiac);
-	}
-		
-		}
-	
-
+	}}
