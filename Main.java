571e90d34da24bfbd699c8131aacd2b201d8e9e7
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-04 11:19:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	
+		int year,divideYear;
+		Scanner input=new Scanner(System.in);
+		System.out.print("Yıl giriniz:");
+		year=input.nextInt();
+		divideYear=year/100;
+		//section that calculates whether the entered year is a leap year
+		if (year%100==0) {
+			 if (divideYear % 4==0 )  {
+				System.out.println(  year + " yılı bir artık yıldır!");
+		}
+			 else {
+				 System.out.println(  year + " yılı bir artık yıl değildir!");
+			 }
+			}
+		else  { 
+			if (year % 4 == 0) {
+			System.out.println(  year + " yılı bir artık yıldır!");}
+			else {
+			System.out.println(  year + " yılı bir artık yıl değildir!");
+		}
+		}
+	}}
